0f2fa802f0d1c781a50b0456ac00821666705686
Raj Vishwakarma <87299771+RajVishwakarma01@users.noreply.github.com>
2021-09-13 11:41:16 +0100

    Create Packaging Cupcakes - MUFFINS3.cpp

---
+++ Packaging Cupcakes - MUFFINS3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int package = n/2;
+	    cout<<package+1<<endl;
+	}
+	return 0;
+}
