e736553dce380a0c2669633e92d524155c80adbb
Raj Vishwakarma <87299771+RajVishwakarma01@users.noreply.github.com>
2021-10-01 09:12:03 +0100

    Create The Smallest Pair - SMPAIR.cpp

---
+++ The Smallest Pair - SMPAIR.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[j];
+        }
+        sort(arr,arr+n);
+        cout<<arr[0]+arr[1]<<endl;
+    }
+    return 0;
+}
