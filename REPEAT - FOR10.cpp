18b7fce6d39d697b8899f74e9dad8bdbc72046c2
Raj Vishwakarma <87299771+RajVishwakarma01@users.noreply.github.com>
2022-03-10 14:24:00 +0530

    Create REPEAT - FOR10.cpp

---
+++ REPEAT - FOR10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,ans;
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        ans = i*n;
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
